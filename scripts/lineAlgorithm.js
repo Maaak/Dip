96fa9032a89311a0a980cd383ee7139a4942ffc1
Maaak <TowstonogAlexander@gmail.com>
2014-12-23 18:00:36 +0200

    Added logic for rectangle calculation

--- scripts/lineAlgorithm.js
+++ scripts/lineAlgorithm.js
@@ -5,24 +5,47 @@ app.lineAlgorithm = function(){
 	LineAlgorithm = function(){
 
 		this.rectangles = [];
+		this.filledRectangles = [];
 	};
 
 	LineAlgorithm.prototype.addEdge = function(fPoint, lPoint){
 		
 		
-		_.find(this.rectangles, function(rect){
-			var buf = rect[0].y <= fPoint.y && rect[1].y >= fPoint.y || rect[0].y <= lPoint.y && rect[1].y >= lPoint.y;
+		var startEdge = _.find(this.rectangles, function(rect){
+			var buf = 	(rect[0].y <= fPoint.y && rect[1].y >= fPoint.y) ||
+						(rect[0].y <= lPoint.y && rect[1].y >= lPoint.y);
 			console.log(buf);
 			return buf;
 		});
 
 
+		if (startEdge) {
+			this.filledRectangles.push({
+				0: startEdge[0],
+				1: startEdge[1],
+				2: fPoint,
+				3: lPoint,
+			});
+			
+			var key;
+			for (key in startEdge) {
+				if(startEdge[key].y === fPoint.y){
 
-		// adding new rectangle
-		this.rectangles.push({
-			0: {fPoint},
-			1: {lPoint},
-		});
+				}else{
+					if(startEdge[key].y === lPoint.y){
+						
+					}	
+				}
+			}
+
+		}else{
+			// adding new rectangle
+			this.rectangles.push({
+				0: fPoint,
+				1: lPoint,
+			});
+		}
+
 	}
 
 
